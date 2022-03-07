2744cb7d036f43ec5d18b36684f3173c874b98f0
Anton Kozyriev <sonara4mus@gmail.com>
2022-03-07 22:42:19 +0200

    feat: extract header as a separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,14 @@
-import {
-  Container, Title, Text, Space,
-  Grid, Blockquote, Image, Divider,
-  Group, Badge
-} from '@mantine/core';
+import { Container } from '@mantine/core';
 import { useState } from 'react';
 import { MantineProvider, ColorSchemeProvider, ColorScheme } from '@mantine/core';
-import SocialMediaBanner from './components/SocialMediaBanner';
 import ThemeSwitch from './components/ThemeSwitch';
 
+import HeaderSection from './sections/HeaderSection';
 import EducationSection from './sections/EducationSection';
 import WorkExperienceSection from './sections/WorkExperienceSection';
 import PersonalProjectsSection from './sections/PersonalProjectsSection';
 
-import profile from './static/profile.png';
-
 function App() {
-  const styles = {
-    profilePicWrapper: {
-      position: "relative"
-    },
-    profilePic: {
-      width: 160, margin: "auto",
-      position: "absolute", right: "1.5rem"
-    },
-    header: {
-      padding: "1.5rem", display: "flex",
-      alignItems: "center", flexDirection: "column"
-    }
-  }
-
-  const skills = [
-    "Software development", "Natural language processing", "Database management (SQL, NoSQL)",
-    "Big Data", "Feature engineering", "Machine learning", "Deep learning", "Data Visualization",
-    "Applied mathematics", "Statistics", "Cloud"
-  ];
-
   const [colorScheme, setColorScheme] = useState<ColorScheme>('light');
   const toggleColorScheme = (value?: ColorScheme) =>
     setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
@@ -44,34 +18,7 @@ function App() {
       <MantineProvider theme={{ colorScheme }} withGlobalStyles>
         <Container>
           <ThemeSwitch />
-          <Divider size={10} color="dark" />
-          <Space h="xl" />
-          <Grid columns={24}>
-            <Grid.Col key={"name"} span={12} style={styles.header as React.CSSProperties}>
-              <Title order={1}>Anton Kozyriev</Title>
-              <Text transform="uppercase">Chief Data Science Officer @ Postello.ai</Text>
-              <Space h="md" />
-              <SocialMediaBanner />
-            </Grid.Col>
-            <Grid.Col key={"profile-photo"} span={12} style={styles.profilePicWrapper as React.CSSProperties}>
-              <div style={styles.profilePic as React.CSSProperties}>
-                <Image radius="xl" src={profile} alt="profile photo" />
-              </div>
-            </Grid.Col>
-          </Grid>
-          <Space h="xs" />
-          <Group spacing="xs">
-              {skills.map(skill => (
-                  <Badge key={skill} variant="gradient" gradient={{ from: 'indigo', to: 'blue' }}>{skill}</Badge>
-              ))}
-          </Group>
-          <Space h="xs" />
-          <Blockquote cite="My summary">
-            I'm a Ukrainian graduate student interested in Web technologies and Big Data.
-            Features that motivate me to develop are unique and challenging tasks, discoveries, and friendly team members.
-            My strengths are <strong>creativity</strong>, <strong>perfectionism</strong>, <strong>competitiveness</strong>, <strong>persistence</strong>, and <strong>focusing on the final result</strong>.
-          </Blockquote>
-          <Space h="xl" />
+          <HeaderSection />
           <EducationSection />
           <WorkExperienceSection />
           <PersonalProjectsSection />
