d2f7b4dec3691ca8eed75039cc692475c16d7243
Anton Kozyriev <sonara4mus@gmail.com>
2022-04-19 00:22:28 +0300

    env: reconfigure environment for tailwind

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 [![DigitalOcean Referral Badge](https://web-platforms.sfo2.digitaloceanspaces.com/WWW/Badge%202.svg)](https://www.digitalocean.com/?refcode=7367a160fb6b&utm_campaign=Referral_Invite&utm_medium=Referral_Program&utm_source=badge)
 
-A static pages site [antonkozyriev.me](https://antonkozyriev.me) contains my contacts, working experience and other personal public information. Built with [React.js](https://reactjs.org/) and [Mantine](https://mantine.dev/).
+A static pages site [antonkozyriev.me](https://antonkozyriev.me) contains my contacts, working experience and other personal public information. Built with [React.js](https://reactjs.org) and [Tailwind.css](https://tailwindcss.com).
 
 ### Attributions
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,20 @@
-import { Container } from '@mantine/core';
-import { useState } from 'react';
-import { MantineProvider, ColorSchemeProvider, ColorScheme } from '@mantine/core';
-import ThemeSwitch from './components/ThemeSwitch';
-
-import HeaderSection from './sections/HeaderSection';
-import EducationSection from './sections/EducationSection';
-import WorkExperienceSection from './sections/WorkExperienceSection';
-import PersonalProjectsSection from './sections/PersonalProjectsSection';
-import CertificationSection from './sections/CertificationSection';
-import LanguageSection from './sections/LanguageSection';
-import FooterSection from './sections/FooterSection';
-
 function App() {
-  const [colorScheme, setColorScheme] = useState<ColorScheme>('light');
-  const toggleColorScheme = (value?: ColorScheme) =>
-    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
-
   return (
-    <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
-      <MantineProvider theme={{ colorScheme }} withGlobalStyles>
-        <Container>
-          <ThemeSwitch />
-          <HeaderSection />
-          <LanguageSection />
-          <EducationSection />
-          <WorkExperienceSection />
-          <CertificationSection />
-          <PersonalProjectsSection />
-          <FooterSection />
-        </Container>
-      </MantineProvider>
-    </ColorSchemeProvider>
+    <div className="flex justify-center">
+      <div className="rounded-lg shadow-lg bg-white max-w-sm">
+        <a href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">
+          <img className="rounded-t-lg" src="https://mdbootstrap.com/img/new/standard/nature/182.jpg" alt=""/>
+        </a>
+        <div className="p-6">
+          <h5 className="text-gray-900 text-xl font-medium mb-2">Card title</h5>
+          <p className="text-gray-700 text-base mb-4">
+            Some quick example text to build on the card title and make up the bulk of the card's
+            content.
+          </p>
+          <button type="button" className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
+        </div>
+      </div>
+    </div>
   );
 }
 
