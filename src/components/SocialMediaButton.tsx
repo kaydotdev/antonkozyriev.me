a91b71ef5765f67c7d09cf96dac67b992b1a3226
Anton Kozyriev <sonara4mus@gmail.com>
2022-04-20 16:30:02 +0300

    feat: social media buttons section implementation

--- src/components/SocialMediaButton.tsx
+++ src/components/SocialMediaButton.tsx
@@ -11,7 +11,7 @@ function SocialMediaButton(props: SocialMediaButtonProps) {
     };
 
     return (
-        <button type="button" className="inline-block px-6 py-2.5 mb-2 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out" style={buttonStyles}>
+        <button type="button" className="inline-block p-2 mb-2 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out" style={buttonStyles}>
             {props.children}
         </button>
     );
