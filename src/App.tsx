17cfbf2b579f8b076054b2b2a81227fb85a15c43
Anton Kozyriev <sonara4mus@gmail.com>
2022-04-20 14:21:48 +0300

    feat(Header.tsx): add icon support to the beginning

--- src/App.tsx
+++ src/App.tsx
@@ -77,7 +77,11 @@ function App() {
       </div>
       <div className="grow bg-slate-100">
         <div className="flex flex-col p-6">
-          <Header text="Work experience" />
+          <Header text="Work experience">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+              <path strokeLinecap="round" strokeLinejoin="round" d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+            </svg>
+          </Header>
           <Timespan sections={workExperienceSection} />
         </div>
       </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,19 @@
+import { ReactNode } from 'react';
+
 interface HeaderProps {
     text: string;
+    children: ReactNode;
 }
 
 function Header(props: HeaderProps) {
     return (
-        <div className="bg-sky-900 uppercase px-4 py-2 mb-2 text-slate-100 font-extrabold w-fit">
-            {props.text}
+        <div className="flex flex-row bg-sky-900 text-slate-100 px-4 py-2 mb-2 w-fit items-center">
+            <div className="h-4 w-4 mr-2">
+                {props.children}
+            </div>
+            <div className="uppercase font-extrabold">
+                {props.text}
+            </div>
         </div>
     );
 }
