4416b7c2f40765a9a8051f8068d0f6bf97e8fedf
Anton Kozyriev <sonara4mus@gmail.com>
2022-04-20 15:00:19 +0300

    fix: make header component responsive to the mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,14 +7,17 @@ interface HeaderProps {
 
 function Header(props: HeaderProps) {
     return (
-        <div className="flex flex-row bg-sky-900 text-slate-100 px-4 py-2 mb-2 w-fit items-center">
-            <div className="h-4 w-4 mr-2">
-                {props.children}
+        <>
+            <div className="hidden sm:flex flex-row bg-sky-900 text-slate-100 px-4 py-2 mb-2 w-fit items-center">
+                <div className="h-4 w-4 mr-2">
+                    {props.children}
+                </div>
+                <div className="uppercase font-extrabold">
+                    {props.text}
+                </div>
             </div>
-            <div className="uppercase font-extrabold">
-                {props.text}
-            </div>
-        </div>
+            <div className="sm:hidden mb-3 font-extrabold uppercase border-b-[1px] sm:border-slate-100 border-gray-800 sm:text-slate-100 text-gray-800">{props.text}</div>
+        </>
     );
 }
 
