58f246f344dfb1c228f9adbbc765a26ac98b5e04
Anton Kozyriev <sonara4mus@gmail.com>
2022-04-20 15:22:54 +0300

    fix: make timespan component text responsive to the mobile

--- src/components/Timespan.tsx
+++ src/components/Timespan.tsx
@@ -16,11 +16,11 @@ function Timespan(props: TimespanProps) {
                     <li>
                         <div className="flex flex-start items-center pt-3">
                             <div className="bg-gray-300 w-2 h-2 rounded-full -ml-1 mr-3"></div>
-                            <p className="text-gray-500 text-sm">{section.timestamp}</p>
+                            <p className="text-gray-500 sm:text-sm text-xs">{section.timestamp}</p>
                         </div>
                         <div className="mt-0.5 ml-4 mb-6">
-                            <h4 className="text-gray-800 font-semibold text-xl mb-1.5">{section.title}</h4>
-                            <ol className="ml-5 mt-2 list-decimal text-gray-500">
+                            <h4 className="text-gray-800 font-semibold sm:text-xl text-base mb-1.5">{section.title}</h4>
+                            <ol className="ml-5 mt-2 list-decimal text-gray-500 sm:text-base text-sm">
                                 {
                                     section.events.map(event => (<li>{event}</li>))
                                 }
