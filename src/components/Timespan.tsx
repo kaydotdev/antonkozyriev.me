e704649814656e71646ce6f9e9587c8fd554f1ee
Anton Kozyriev <sonara4mus@gmail.com>
2022-04-21 15:52:20 +0300

    feat(Timespan): responsive colors for mobile devices

--- src/components/Timespan.tsx
+++ src/components/Timespan.tsx
@@ -10,17 +10,17 @@ interface TimespanProps {
 
 function Timespan(props: TimespanProps) {
     return (
-        <ol className="border-l border-sky-900">
+        <ol className="border-l border-gray-300 sm:border-sky-900">
         {
             props.sections.map(section => (
                 <li>
                     <div className="flex flex-start items-center pt-3">
-                        <div className="bg-sky-900 w-2 h-2 rounded-full -ml-1 mr-3"></div>
-                        <p className="text-gray-800 sm:text-sm text-xs">{section.timestamp}</p>
+                        <div className="bg-gray-300 sm:bg-sky-900 w-2 h-2 rounded-full -ml-1 mr-3"></div>
+                        <p className="text-gray-500 sm:text-gray-800 sm:text-sm text-xs">{section.timestamp}</p>
                     </div>
                     <div className="ml-4 mb-2">
                         <h4 className="text-gray-800 font-semibold sm:text-xl text-base mb-1.5">{section.title}</h4>
-                        <ol className="ml-5 mt-2 list-disc text-gray-800 sm:text-sm text-xs">
+                        <ol className="ml-5 mt-2 list-disc text-gray-500 sm:text-gray-800 sm:text-sm text-xs">
                         {
                             section.events.map(event => (<li>{event}</li>))
                         }
