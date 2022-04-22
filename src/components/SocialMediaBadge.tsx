bcbc261b45c01dbd701a35d902a048e7734cf8d2
Anton Kozyriev <sonara4mus@gmail.com>
2022-04-22 16:42:40 +0300

    fix(SocialMediaBadge.tsx): resolve icon bug with word breaking

--- src/components/SocialMediaBadge.tsx
+++ src/components/SocialMediaBadge.tsx
@@ -9,7 +9,7 @@ function SocialMediaBadge(props: SocialMediaBadgeProps) {
     return (
         <div className="flex flex-row items-center mb-2.5">
             {props.children}
-            <p className="text-xs font-base tracking-tight ml-3">{props.href}</p>
+            <p className="text-xs font-base tracking-tight ml-3 break-all">{props.href}</p>
         </div>
     );
 }
