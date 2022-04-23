78cf8ecb21b18148fd7a999331d307764a7a55e2
Anton Kozyriev <sonara4mus@gmail.com>
2022-04-23 22:08:14 +0300

    feat: add charity refs to the #StandWithUkraine widget

--- src/components/UkraineCharityWidget.tsx
+++ src/components/UkraineCharityWidget.tsx
@@ -12,7 +12,7 @@ function UkraineCharityWidget() {
             <div className="flex space-x-2 justify-center">
                 <div className="bg-white shadow-lg mx-auto w-72 max-w-full text-sm pointer-events-auto bg-clip-padding rounded-lg block">
                     <div className=" bg-white flex justify-between items-center py-2 px-3 bg-clip-padding border-b border-gray-200 rounded-t-lg">
-                        <p className="font-bold text-gray-800 tracking-tigher uppercase">{invasionDays} days of invasion</p>
+                        <p className="text-gray-800 tracking-tigher"><span className="font-extrabold">{invasionDays}</span> days of invasion</p>
                         <div className="flex items-center">
                             <button onClick={() => setHidden(true)} type="button" className="box-content w-4 h-4 ml-2 text-gray-800 border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-gray-800 hover:opacity-75 hover:no-underline">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
@@ -21,15 +21,21 @@ function UkraineCharityWidget() {
                             </button>
                         </div>
                     </div>
-                    <div className="p-3 bg-white rounded-b-lg break-words text-gray-700">
-                    <div className="flex flex-row justify-between items-center gap-x-2 mb-2">
-                        <div className="flex-none">
-                            <img src={MotherlandFlag} className="w-10 h-10 rounded-md object-cover" alt="Flag" /></div>
+                    <div className="p-3 bg-white rounded-b-lg break-words text-gray-700 border-b border-gray-200">
+                        <div className="flex flex-row justify-between items-center gap-x-2">
+                            <div className="flex-none">
+                                <img src={MotherlandFlag} className="w-10 h-10 rounded-md object-cover" alt="Flag" />
+                            </div>
                             <div className="grow">
-                                <p className="text-medium font-extrabold uppercase">Support Ukraine</p>
+                                <a href="https://war.ukraine.ua" className="text-medium font-extrabold uppercase underline">Support Ukraine</a>
                                 <p className="text-xs tracking-tight">Stop Russian Aggression!</p>
                             </div>
                         </div>
+                    </div>
+                    <div className="p-3 flex flex-row justify-evenly items-center gap-x-2">
+                        <a href="https://war.ukraine.ua/support-ukraine" className="text-blue-600 hover:text-blue-700 transition duration-300 ease-in-out underline uppercase">Support</a>
+                        <a href="https://war.ukraine.ua/donate" className="text-yellow-500 hover:text-yellow-600 transition duration-300 ease-in-out underline uppercase">Donate</a>
+                        <a href="https://war.ukraine.ua/arm-ukraine-now" className="text-gray-800 hover:text-gray-900 transition duration-300 ease-in-out underline uppercase">Arm Ukraine</a>
                     </div>
                 </div>
             </div>
