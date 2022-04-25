89294e4e9d2104c4eea0143e6cba0909faf8d099
Anton Kozyriev <sonara4mus@gmail.com>
2022-04-25 22:39:28 +0300

    feat: embed qr code info resume print version

--- src/components/Divider.tsx
+++ src/components/Divider.tsx
@@ -4,7 +4,7 @@ interface DividerProps {
 
 function Divider(props: DividerProps) {
     return (
-        <div className="font-extrabold uppercase ml-6 mr-6 border-b-[1px] sm:border-slate-100 border-gray-800 sm:text-slate-100 text-gray-800">{props.text}</div>
+        <div className="font-extrabold print:text-sm uppercase ml-6 mr-6 border-b-[1px] sm:border-slate-100 border-gray-800 sm:text-slate-100 text-gray-800">{props.text}</div>
     );
 }
 
