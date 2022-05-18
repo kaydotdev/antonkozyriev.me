0301f321f5710368811eddc712c21394ff092318
Anton Kozyriev <sonara4mus@gmail.com>
2022-05-18 16:27:43 +0300

    feat(index.tsx): auto switch of theme using 'prefers-color-scheme'

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+    const pageRoot = document.querySelector('html');
+    if (!!pageRoot) { pageRoot.className = event.matches ? "dark" : "light"; }
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(<React.StrictMode><App /></React.StrictMode>);
 
