a8b7ae0fe0dcceb9b891872e832da41fd17b4425
Anton Kozyriev <sonara4mus@gmail.com>
2022-05-18 16:42:07 +0300

    feat: match theme in page init

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
-    const pageRoot = document.querySelector('html');
-    if (!!pageRoot) { pageRoot.className = event.matches ? "dark" : "light"; }
-});
+const matchTheme = (matches: boolean) => { const pageRoot = document.querySelector('html'); if (!!pageRoot) { pageRoot.className = matches ? "dark" : "light"; } }
+
+matchTheme(window.matchMedia('(prefers-color-scheme: dark)').matches);
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => matchTheme(event.matches));
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(<React.StrictMode><App /></React.StrictMode>);
