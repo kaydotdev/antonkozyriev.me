dd480e6aaeb66df974f570b1540a3f819c1c92a0
Anton Kozyriev <sonara4mus@gmail.com>
2022-10-06 21:27:58 +0300

    build: restructure project for astro framework

--- src/utils/theme.ts
+++
@@ -1,7 +0,0 @@
-export function matchTheme(matches: boolean) {
-    const pageRoot = document.querySelector('html');
-
-    if (!!pageRoot) {
-        pageRoot.className = matches ? "dark" : "light";
-    }
-}
