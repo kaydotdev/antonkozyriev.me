dcd8abd3a18e9fc39c1cacb2d3dc5d5d3002aead
Anton Kozyriev <sonara4mus@gmail.com>
2022-12-21 18:28:18 +0200

    feat: page slide component

---
+++ src/routes/Slide.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	export let id: string;
+	export let align: boolean = false;
+</script>
+
+<div {id} class={align ? 'slide slide-align' : 'slide'}>
+	<slot />
+</div>
+
+<style>
+	div.slide {
+		display: flex;
+		flex-direction: column;
+		width: 100vw;
+		height: 100vh;
+	}
+
+	.slide-align {
+		justify-content: center;
+		align-items: center;
+	}
+</style>
