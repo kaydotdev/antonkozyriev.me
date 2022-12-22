49430f95959baff14fc30e4ec2124f83b27a70aa
Anton Kozyriev <sonara4mus@gmail.com>
2022-12-22 19:33:28 +0200

    fix: move global styles to external stylesheet

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import '$lib/styles/global.css';
 	import Slide from './Slide.svelte';
 	import ProfileCard from './ProfileCard.svelte';
 </script>
@@ -12,18 +13,3 @@
 <Slide id="experience" />
 <Slide id="certification" />
 <Slide id="portfolio" />
-
-<style>
-	@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');
-
-	:global(body) {
-		margin: 0;
-		overflow: hidden;
-		font-family: 'Inter', sans-serif;
-		background-color: #ffffff;
-	}
-
-	:global(h1) {
-		font-weight: 900;
-	}
-</style>
