b35fefc1c6e8f10a632b5865673c7af4a2ee3dea
Anton Kozyriev <sonara4mus@gmail.com>
2023-03-05 21:27:29 +0200

    feat: particles animation on the wallpaper

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,61 +1,63 @@
 <script>
-	import profile from '$lib/assets/profile.png';
-	import Image from '$lib/components/Image.svelte';
-	import SocialBar from '$lib/components/SocialBar.svelte';
-	import Slide from '$lib/components/Slide.svelte';
+	import wallpaperSrc from '$lib/assets/wallpaper.png';
+	import particlesConfig from '$lib/config/particlesjs.json';
+	import Particles from 'svelte-particles';
+	import { loadFull } from 'tsparticles';
+	import '$lib/styles/normalize.css';
 	import '$lib/styles/global.css';
+
+	$: bgImage = `background-image: url("${wallpaperSrc}");`;
+
+	let onParticlesLoaded = (event) => {
+		const particlesContainer = event.detail.particles;
+	};
+	let particlesInit = async (engine) => {
+		await loadFull(engine);
+	};
 </script>
 
-<Slide>
-	<div class="stretch center">
-		<div class="profile-card">
-			<Image
-				style="border-radius: 9999px; width: 8rem; height: 8rem; filter: drop-shadow(0 10px 8px rgb(0 0 0 / 0.04)) drop-shadow(0 4px 3px rgb(0 0 0 / 0.1));"
-				width={400}
-				height={400}
-				hash="USHxNW.9%~-p~VkDXSWW0fM_VYRjtQt6xZs:"
-				src={profile}
-				imgAlt="Profile pic"
-			/>
-			<h1>Anton Kozyriev</h1>
-			<small>Data Scientist @ Postello.ai</small>
-			<p>
-				"I am a Ukrainian graduate student and a professional Machine Learning Engineer / Software
-				Developer with 3+ of experience in both academic and enterprise. My development and research
-				interests are Deep Learning, Natural Language Processing and Big Data. I am looking for a
-				projects with unique and challenging tasks, discoveries, and friendly team members."
-			</p>
-			<SocialBar />
-		</div>
+<div class="full-screen background">
+	<div style={bgImage} class="stretch background-image" />
+	<div class="stretch background-mask">
+		<Particles
+			id="tsparticles"
+			options={particlesConfig}
+			on:particlesLoaded={onParticlesLoaded}
+			{particlesInit}
+		/>
 	</div>
-</Slide>
+	<div class="stretch background-content" />
+</div>
 
 <style>
-	.profile-card {
-		max-width: 28rem;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
+	.background-image {
+		background-repeat: no-repeat;
+		background-size: cover;
+		-webkit-background-size: cover;
+		-moz-background-size: cover;
+		z-index: 0;
 	}
 
-	h1 {
-		margin-top: 1rem;
-		margin-bottom: 0.5rem;
+	.background-mask {
+		top: 0;
+		left: 0;
+		opacity: 0.6;
+		background-color: #000000;
+		position: absolute;
+		z-index: 1;
 	}
 
-	small {
-		font-size: 0.65rem;
-		text-transform: uppercase;
-		letter-spacing: 0.25em;
-		color: #0009;
-	}
-
-	p {
-		font-size: 0.75rem;
-		margin: 2rem 1rem;
-		font-weight: 200;
-		font-style: italic;
-		text-align: justify;
-		letter-spacing: -0.025em;
+	.background-content {
+		top: 0;
+		left: 0;
+		position: absolute;
+		z-index: 2;
+		color: white;
+		font-weight: 300;
+		font-size: 1.25rem;
+		line-height: 1.75rem;
+		letter-spacing: 0.025em;
+		-webkit-font-smoothing: auto;
+		-moz-osx-font-smoothing: auto;
 	}
 </style>
