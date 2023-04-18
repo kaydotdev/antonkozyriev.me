01dc07f0bdd0f1ae2d50c7752542aba12c61ed4a
Anton Kozyriev <sonara4mus@gmail.com>
2023-04-18 15:36:16 +0300

    feat(tailwind.config.cjs): extend color scheme with custom values

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
-	theme: {},
+	theme: {
+		extend: {
+			colors: {
+				'primary': '#4A4A4A',
+				'secondary': '#CDB8A7',
+				'ascent': '#FCF8F3'
+			}
+		}
+	},
 	plugins: []
 };
