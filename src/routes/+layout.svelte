7c8fd7edb55f4d1e371b97a87e4a3e7564ab9d7f
Anton Kozyriev <sonara4mus@gmail.com>
2023-04-19 19:22:14 +0300

    fix(+layout.svelte): handle device unsupported exception for orientation lock

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,9 @@
 	import { onMount } from 'svelte';
 	import '../app.css';
 
-	// Prevent screen rotation on mobile devices
 	onMount(async () => {
 		if ('orientation' in screen) {
-			await screen.orientation.lock('portrait');
+			try { await screen.orientation.lock('portrait') } catch {}
 		}
 	});
 </script>
