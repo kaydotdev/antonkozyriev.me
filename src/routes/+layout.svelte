d38608a5c3d2cc5c4ab3f7ca49f833eee18f1998
Anton Kozyriev <sonara4mus@gmail.com>
2023-04-19 18:44:32 +0300

    feat(+layout.svelte): lock screen rotation on the mobile devices

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,13 @@
 <script>
+	import { onMount } from 'svelte';
 	import '../app.css';
+
+	// Prevent screen rotation on mobile devices
+	onMount(async () => {
+		if ('orientation' in screen) {
+			await screen.orientation.lock('portrait');
+		}
+	});
 </script>
 
 <slot />
